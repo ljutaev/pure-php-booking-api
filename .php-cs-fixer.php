757bedc9e8e3a0bc40c320abd54c119050b0ea63
ljutaev <ljutaev@gmail.com>
2026-04-28 00:20:50 +0200

    chore: add PHPUnit, PHPStan, PHP CS Fixer config

---
+++ .php-cs-fixer.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'trailing_comma_in_multiline' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in([__DIR__ . '/src', __DIR__ . '/tests'])
+    );
